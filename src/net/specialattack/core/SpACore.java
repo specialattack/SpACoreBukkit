6e3105154dedd98d982bb8f48609765325ef3eab
Heldplayer <mitchel.pyl@gmail.com>
2012-12-05 14:06:34 +0100

    Add logger functions to main plugin class + base of playground system

--- src/net/specialattack/core/SpACore.java
+++ src/net/specialattack/core/SpACore.java
@@ -1,15 +1,27 @@
 
 package net.specialattack.core;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpACore extends JavaPlugin {
     public static SpACore instance;
     public static PluginState state = PluginState.Unloaded;
+    protected int lastId;
+    protected PluginDescriptionFile pdf;
+    private Logger logger;
 
     public SpACore() {
         super();
         state = PluginState.Initializing;
+
+        instance = this;
+
+        logger = this.getLogger();
+
         state = PluginState.Initialized;
     }
 
@@ -24,6 +36,10 @@ public class SpACore extends JavaPlugin {
     public void onEnable() {
         state = PluginState.Enabling;
 
+        this.pdf = this.getDescription();
+
+        this.lastId = 0;
+
         state = PluginState.Enabled;
     }
 
@@ -32,5 +48,21 @@ public class SpACore extends JavaPlugin {
         state = PluginState.Disabling;
 
         state = PluginState.Disabled;
+    }
+
+    public static int getNextAvailablePlaygroundId() {
+        return instance.lastId++;
+    }
+
+    public static void log(String message) {
+        instance.logger.log(Level.INFO, message);
+    }
+
+    public static void log(Level level, String message) {
+        instance.logger.log(level, message);
+    }
+
+    public static void log(Level level, String message, Throwable throwable) {
+        instance.logger.log(level, message, throwable);
     }
 }
