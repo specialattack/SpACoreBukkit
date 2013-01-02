92e7f244062e2f9241eb7f21f8dbfebe092b050f
Heldplayer <mitchel.pyl@gmail.com>
2013-01-02 11:32:28 +0100

    Cleanup PlayerClass, made it abstract

---
+++ src/net/specialattack/core/games/classes/AbstractPlayer.java
@@ -0,0 +1,47 @@
+
+package net.specialattack.core.games.classes;
+
+import java.util.List;
+import java.util.logging.Level;
+
+import net.specialattack.core.PlayerStorage;
+import net.specialattack.core.SpACore;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public abstract class AbstractPlayer {
+
+    public final String name;
+    public boolean errored = false; // Use me to determine if the player is still playing, if this is true the player shouldn't be messed with
+
+    public AbstractPlayer(String name) {
+        this.name = name;
+    }
+
+    public Player tryGetPlayer() {
+        return Bukkit.getPlayer(this.name);
+    }
+
+    public abstract void addToLoadout(ItemStack equiptment);
+
+    public abstract List<ItemStack> getLoadout();
+
+    public void loadoutPlayer() {
+        Player player = this.tryGetPlayer();
+
+        try {
+            PlayerStorage.store(SpACore.instance, player);
+            this.doLoadout();
+        }
+        catch (Exception e) {
+            this.errored = true;
+            player.sendMessage(ChatColor.RED + "An error occoured, please contact mbl111 or heldplayer");
+            SpACore.log(Level.SEVERE, "Error occoured while trying to save player inventory", e);
+        }
+    }
+
+    protected abstract void doLoadout();
+}
