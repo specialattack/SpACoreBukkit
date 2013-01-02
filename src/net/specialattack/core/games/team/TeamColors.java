95cf407007d70dde517c2662261bde9086bba4b9
Heldplayer <mitchel.pyl@gmail.com>
2013-01-02 11:20:26 +0100

    Makes mbl's color blu

--- src/net/specialattack/core/games/team/TeamColors.java
+++ src/net/specialattack/core/games/team/TeamColors.java
@@ -5,26 +5,28 @@ import org.bukkit.ChatColor;
 
 public enum TeamColors {
 
-    BLACK(15, ChatColor.BLACK), //
-    RED(14, ChatColor.DARK_RED), //
-    GREEN(13, ChatColor.DARK_GREEN), //
-    BLUE(11, ChatColor.DARK_BLUE), //
-    PURPLE(10, ChatColor.DARK_PURPLE), //
-    DARK_AQUA(9, ChatColor.DARK_AQUA), //
-    LIGHT_GREY(8, ChatColor.GRAY), //
-    DARK_GREY(7, ChatColor.DARK_GRAY), //
-    LIME(5, ChatColor.GREEN), //
-    YELLOW(4, ChatColor.YELLOW), //
-    LIGHT_PURPLE(2, ChatColor.LIGHT_PURPLE), //
-    ORANGE(1, ChatColor.GOLD), //
-    WHITE(0, ChatColor.WHITE);
+    BLACK(15, ChatColor.BLACK, 0x000000), //
+    RED(14, ChatColor.DARK_RED, 0xFF0000), //
+    GREEN(13, ChatColor.DARK_GREEN, 0x007800), //
+    BLUE(11, ChatColor.DARK_BLUE, 0x0000FF), //
+    PURPLE(10, ChatColor.DARK_PURPLE, 0x7F007F), //
+    DARK_AQUA(9, ChatColor.DARK_AQUA, 0x007F7F), //
+    LIGHT_GREY(8, ChatColor.GRAY, 0x7F7F7F), //
+    DARK_GREY(7, ChatColor.DARK_GRAY, 0x3F3F3F), //
+    LIME(5, ChatColor.GREEN, 0x00FF00), //
+    YELLOW(4, ChatColor.YELLOW, 0xFFFF00), //
+    LIGHT_PURPLE(2, ChatColor.LIGHT_PURPLE, 0xFF00FF), //
+    ORANGE(1, ChatColor.GOLD, 0xFF7F00), //
+    WHITE(0, ChatColor.WHITE, 0xFFFFFF);
 
     private int blockData;
     private ChatColor chatColor;
+    private int armorColor;
 
-    private TeamColors(int blockData, ChatColor chatColor) {
+    private TeamColors(int blockData, ChatColor chatColor, int armorColor) {
         this.blockData = blockData;
         this.chatColor = chatColor;
+        this.armorColor = armorColor;
     }
 
     public int getBlockData() {
@@ -33,6 +35,10 @@ public enum TeamColors {
 
     public ChatColor getChatColor() {
         return chatColor;
+    }
+
+    public int getArmorColor() {
+        return armorColor;
     }
 
 }
