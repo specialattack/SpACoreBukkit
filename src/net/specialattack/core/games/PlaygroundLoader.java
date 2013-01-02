aa2dac642bcdfd0888d65762ed4a93fee050dbd8
Heldplayer <mitchel.pyl@gmail.com>
2013-01-02 13:03:35 +0100

    Made PlaygroundLoader an interface

--- src/net/specialattack/core/games/PlaygroundLoader.java
+++ src/net/specialattack/core/games/PlaygroundLoader.java
@@ -3,6 +3,6 @@ package net.specialattack.core.games;
 
 import net.specialattack.core.block.Cuboid;
 
-public abstract class PlaygroundLoader {
+public interface PlaygroundLoader {
     public abstract Playground createInstance(Cuboid cuboid);
 }
